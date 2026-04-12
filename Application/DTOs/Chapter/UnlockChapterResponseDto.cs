0e21e26486acb2b19b84a00c6cea8ba545e6f705
Miad04 <d.ami111004@gmail.com>
2026-04-13 05:43:39 +0700

    fixing 5 minute cache on chapter upload/purchase, fixing transaction logic

--- Application/DTOs/Chapter/UnlockChapterResponseDto.cs
+++ Application/DTOs/Chapter/UnlockChapterResponseDto.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Application.DTOs.Chapter
 {
-  public class UnlockChapterResponseDto
-  {
-    public int ChapterId { get; set; }
-    public long CoinsSpent { get; set; }
-    public decimal NewCoinBalance { get; set; }
-    public string Message { get; set; } = string.Empty;
-  }
+    public class UnlockChapterResponseDto
+    {
+        public int ChapterId { get; set; }
+        public decimal CoinsSpent { get; set; }
+        public decimal NewCoinBalance { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
 }
